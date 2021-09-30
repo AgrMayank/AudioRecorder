2ceb7c0940b7f1aeabb7bff72dad0fa0c9030bfd
Mayank Raj <AgrMayank@outlook.com>
2021-09-30 13:57:47 +0530

    Add Audio Player Scripts, UI

---
+++ Assets/Recorder/AudioPlayer.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using TMPro;
+
+public class AudioPlayer : MonoBehaviour
+{
+    public Slider audioSlider;
+    public AudioSource audioSource;
+
+    [HideInInspector]
+    public AudioClip audioClip;
+
+    public TMP_Text ConsoleText;
+
+    private bool isPlaying = false;
+
+    public void PlayAudio()
+    {
+        if (!isPlaying)
+        {
+            audioSource.clip = audioClip;
+
+            audioSlider.direction = Slider.Direction.LeftToRight;
+            audioSlider.minValue = 0;
+            audioSlider.maxValue = audioSource.clip.length;
+
+            audioSource.Play();
+            isPlaying = true;
+        }
+        else
+        {
+            audioSource.UnPause();
+        }
+    }
+
+    public void PauseAudio()
+    {
+        audioSource.Pause();
+    }
+
+    void Update()
+    {
+        if (audioClip == null)
+        {
+            ConsoleText.text = "No Audio Clip Found. Record An Audio Clip First.";
+        }
+
+        audioSlider.value = GetComponent<AudioSource>().time;
+
+        if ((audioSlider.value == audioSlider.maxValue || audioSlider.value == audioSlider.minValue) && !audioSource.isPlaying)
+        {
+            isPlaying = false;
+        }
+    }
+}

502ea876374d3cf82a1e614262fba2ab13d91af7
unknown <abolfazlhosnian@gmail.com>
2023-08-07 14:16:13 +0330

    fixing some comments

--- Assets/AudioRecorder/Scripts/Runtime/Player/AudioPlayer.cs
+++ Assets/AudioRecorder/Scripts/Runtime/Player/AudioPlayer.cs
@@ -10,12 +10,12 @@ namespace Mayank.AudioRecorder.Player
     public class AudioPlayer : MonoBehaviour
     {
         /// <summary>
-        /// This slider is used to display the current playback position of the audio file and allows the user to seek to a specific position in the audio file.
+        /// Displays the current playback position and allows seeking.
         /// </summary>
         [SerializeField] private Slider audioSlider;
         
         /// <summary>
-        /// This component is responsible for playing back the audio clip assigned to it and provides various options for modifying the audio playback, such as volume, pitch, and spatialization.
+        /// Plays back the assigned audio clip.
         /// </summary>
         [SerializeField] private AudioSource audioSource;
         
@@ -30,12 +30,12 @@ namespace Mayank.AudioRecorder.Player
         [SerializeField] private Image pauseImage;
         
         /// <summary>
-        /// This AudioClip represents the audio file that will be played back by the AudioSource component. It contains the actual sound data that will be heard during audio playback.
+        /// The audio file that will be played.
         /// </summary>
         [HideInInspector] public AudioClip audioClip;
         
         /// <summary>
-        /// This boolean variable is used to track the playback state of the audio. When set to true, it means the audio is currently paused, and when set to false, it means the audio is playing.
+        /// Tracks the playback state.
         /// </summary>
         private bool _isPaused = false;
         
@@ -46,8 +46,7 @@ namespace Mayank.AudioRecorder.Player
         #region MonoCallbacks
         
         /// <summary>
-        /// Initializes the audio source and UI elements when the script starts.
-        /// This method retrieves the AudioSource component attached to the same GameObject and initializes the necessary variables and UI elements for audio playback.
+        /// Initializes the audio source and UI elements.
         /// </summary>
         private void Start()
         {
@@ -58,7 +57,6 @@ namespace Mayank.AudioRecorder.Player
 
         /// <summary>
         /// Updates the audio playback and UI elements based on the current state.
-        /// This method is called every frame and handles the logic for updating the audio playback and UI elements, such as the audio slider and play/pause images.
         /// </summary>
         private void Update()
         {
@@ -76,10 +74,7 @@ namespace Mayank.AudioRecorder.Player
         #endregion MonoCallbacks
 
         /// <summary>
-        /// Sets the value of the audio slider based on the current playback time of the audio source.
-        /// This method updates the value of the audio slider to reflect the current playback time of the audio source.
-        /// It retrieves the current playback time from the audio source and sets the value of the slider to match it.
-        /// This allows the user to see the current playback position of the audio file and seek to a specific position if needed.
+        /// Sets the value of the audio slider based on the current playback time.
         /// </summary>
         private void SetAudioSliderValue()
         {
@@ -88,9 +83,8 @@ namespace Mayank.AudioRecorder.Player
 
         /// <summary>
         /// Updates the play and pause images to reflect the current playback state.
-        /// This method sets the play and pause images to be active or inactive based on the provided playing status. If the audio is currently playing, the pause image is set to active, and the play image is set to inactive. If the audio is currently paused, the play image is set to active, and the pause image is set to inactive. This provides a visual indication to the user of the current playback state and allows them to control the audio playback using the associated buttons.
         /// </summary>
-        /// <param name="isPlaying">Represents if the audio is playing or not.</param>
+        /// <param name="isPlaying">Indicates if the audio is currently playing or not.</param>
         private void SetPlayPauseImagesStatus(bool isPlaying)
         {
             playImage.gameObject.SetActive(!isPlaying);
@@ -99,7 +93,6 @@ namespace Mayank.AudioRecorder.Player
 
         /// <summary>
         /// Changes the AudioClip of the AudioSource and adjusts the audio slider parameters accordingly.
-        /// This method updates the AudioClip of the AudioSource component to the provided audioClip and adjusts the audio slider parameters to match the length of the new audio clip. It sets the minimum value of the audio slider to 0 and the maximum value to the length of the new audio clip. Additionally, it sets the direction of the audio slider to left-to-right to match the audio playback direction.
         /// </summary>
         public void UpdateClip()
         {
@@ -113,11 +106,6 @@ namespace Mayank.AudioRecorder.Player
 
         /// <summary>
         /// Toggles the audio playback between play and pause based on the current state.
-        /// This method toggles the audio playback between play and pause based on the current state.
-        /// If the audio is currently playing, it pauses the audio playback and sets the play/pause images to the inactive state.
-        /// If the audio is currently paused, it resumes the audio playback and sets the play/pause images to the active state.
-        /// If the audio is not playing, it starts the audio playback and sets the play/pause images to the active state.
-        /// This provides a way for the user to control the audio playback using the associated buttons and provides a visual indication of the current playback state.
         /// </summary>
         public void PlayPauseAudio()
         {
@@ -141,7 +129,5 @@ namespace Mayank.AudioRecorder.Player
                 audioSource.Play();
             }
         }
-
-        
     }
 }

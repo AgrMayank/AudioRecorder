0bd9de2afb7794fcd4968acc7091314eec482eea
unknown <abolfazlhosnian@gmail.com>
2023-08-07 11:35:09 +0330

    some comments added

--- Assets/AudioRecorder/Scripts/Runtime/Player/AudioPlayer.cs
+++ Assets/AudioRecorder/Scripts/Runtime/Player/AudioPlayer.cs
@@ -8,29 +8,89 @@ namespace Mayank.AudioRecorder.Player
     // ToDo: Load last audio file.
     public class AudioPlayer : MonoBehaviour
     {
+        /// <summary>
+        /// The slider which represents the passed time of the audio file as well as whole time and remaining time of the audio file.
+        /// </summary>
         [SerializeField] private Slider audioSlider;
+        
+        /// <summary>
+        /// The AudioSource component which controls audio file  playback.
+        /// </summary>
         [SerializeField] private AudioSource audioSource;
+        
+        /// <summary>
+        /// The image which shown when the audio is paused. It represents that the audio will play if user press the button.
+        /// </summary>
         [SerializeField] private Image playImage;
+        
+        /// <summary>
+        /// The image which shown when the audio is playing. It represents that the audi will pause if user press the button.
+        /// </summary>
         [SerializeField] private Image pauseImage;
-
+        
+        /// <summary>
+        /// The AudioClip that will play in the AudiSource component.
+        /// </summary>
         [HideInInspector] public AudioClip audioClip;
         
+        /// <summary>
+        /// Represents if the audio is playing or not.
+        /// </summary>
         private bool _isPaused = false;
+        
+        
+        // ToDo: Check it: what is the following variable doing? Why does it exists?
         private AudioSource _audioSource;
 
+        #region MonoCallbacks
+        
         private void Start()
         {
             _audioSource = GetComponent<AudioSource>();
             _isPaused = false;
-            IsPlaying(false);
+            SetPlayPauseImagesStatus(false);
         }
 
-        private void IsPlaying(bool isPlaying)
+        private void Update()
+        {
+            // audioSlider.value = _audioSource.time;
+
+            if (audioSource.isPlaying) SetAudioSliderValue();
+            
+            
+            
+            
+            if ((audioSlider.value <= audioSlider.maxValue 
+                 && audioSlider.value >= audioSlider.minValue) ||
+                audioSource.isPlaying) return;
+            SetPlayPauseImagesStatus(false);
+            _isPaused = false;
+        }
+        
+        #endregion MonoCallbacks
+
+        /// <summary>
+        /// Sets the value of the audio slider.
+        /// </summary>
+        private void SetAudioSliderValue()
+        {
+            audioSlider.value = _audioSource.time;
+        }
+
+        /// <summary>
+        /// Sets the play and pause images active or inactive.
+        /// </summary>
+        /// <param name="isPlaying"></param>
+        // private void IsPlaying(bool isPlaying)
+        private void SetPlayPauseImagesStatus(bool isPlaying)
         {
             playImage.gameObject.SetActive(!isPlaying);
             pauseImage.gameObject.SetActive(isPlaying);
         }
 
+        /// <summary>
+        /// Changes the AudioClip of the AudioSource.
+        /// </summary>
         public void UpdateClip()
         {
             audioSource.clip = audioClip;
@@ -39,34 +99,29 @@ namespace Mayank.AudioRecorder.Player
             audioSlider.maxValue = audioSource.clip.length;
         }
 
+        /// <summary>
+        /// Plays or pauses the audio.
+        /// </summary>
         public void PlayPauseAudio()
         {
             if (audioSource.isPlaying)
             {
                 audioSource.Pause();
-                IsPlaying(false);
+                SetPlayPauseImagesStatus(false);
                 _isPaused = true;
             }
             else if (_isPaused)
             {
-                IsPlaying(true);
+                SetPlayPauseImagesStatus(true);
                 audioSource.UnPause();
             }
             else
             {
-                IsPlaying(true);
+                SetPlayPauseImagesStatus(true);
                 audioSource.Play();
             }
         }
 
-        void Update()
-        {
-            audioSlider.value = _audioSource.time;
-            if ((audioSlider.value <= audioSlider.maxValue 
-                 && audioSlider.value >= audioSlider.minValue) ||
-                audioSource.isPlaying) return;
-            IsPlaying(false);
-            _isPaused = false;
-        }
+        
     }
 }

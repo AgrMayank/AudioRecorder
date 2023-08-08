379e5ec6afc5c3626fb23308a06e2370c362cfba
unknown <abolfazlhosnian@gmail.com>
2023-08-08 15:03:44 +0330

    some comments added

--- Assets/AudioRecorder/Scripts/Runtime/Utility/FileWritingResultModel.cs
+++ Assets/AudioRecorder/Scripts/Runtime/Utility/FileWritingResultModel.cs
@@ -1,11 +1,20 @@
 namespace Mayank.AudioRecorder.Utility
 {
-    
-    // ToDo: Add comments
     public class FileWritingResultModel
     {
+        /// <summary>
+        /// The status of the file writing operation.
+        /// </summary>
         public bool status;
+        
+        /// <summary>
+        /// The result of the file writing operation.
+        /// </summary>
         public string result = null;
+        
+        /// <summary>
+        /// The error message, if any, encountered during the file writing operation.
+        /// </summary>
         public string error = null;
     }
 }

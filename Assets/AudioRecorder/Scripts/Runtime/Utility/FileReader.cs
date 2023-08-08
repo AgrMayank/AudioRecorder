aca5255e2dfc18a1c5baca1189891ee1490d70b0
unknown <abolfazlhosnian@gmail.com>
2023-08-08 13:37:59 +0330

    some comments addedd

--- Assets/AudioRecorder/Scripts/Runtime/Utility/FileReader.cs
+++ Assets/AudioRecorder/Scripts/Runtime/Utility/FileReader.cs
@@ -2,10 +2,15 @@ using System.IO;
 
 namespace Mayank.AudioRecorder.Utility
 {
-    
-    // ToDo: Add comments
     public static class FileReader
     {
+        /// <summary>
+        /// Reads an audio file at the specified path and returns it as a byte array.
+        /// </summary>
+        /// <param name="filePath">The path of the audio file to read.</param>
+        /// <param name="audioSourceSamples">The number of audio source samples.</param>
+        /// <param name="headerSize">The size of the audio file header.</param>
+        /// <returns>A byte array containing the audio data.</returns>
         public static byte[] ConvertWavToByteArray(string filePath, int audioSourceSamples, int headerSize)
         {
             var bytes = new byte[audioSourceSamples + headerSize];
